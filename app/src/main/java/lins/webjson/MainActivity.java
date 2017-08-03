e52cc6df3980ccd881f8bb0aab9cca908e80c301
lins <753392431@qq.com>
2017-08-03 15:40:06 +0800

    Initial commit

---
+++ app/src/main/java/lins/webjson/MainActivity.java
@@ -0,0 +1,14 @@
+package lins.webjson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //15.38事件版本
+    }
+}
